5aa76f7b4eaff4d19d55b9c53842f4b270a4c575
Alaa Shalabi <al1996sh.9@gmail.com>
2023-09-13 02:51:41 +0300

    make migrations

---
+++ database/migrations/2023_09_12_225905_create_tasks_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamp('date');
+            $table->float('cost')->default(0);
+            $table->unsignedBigInteger('sub_specialty_id');
+            $table->foreign('sub_specialty_id')
+                ->references('id')->on('sub_specialties')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
